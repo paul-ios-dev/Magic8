e19d1cf95195abdb3b6af4081cac28884cc939cf
sabg@paul-kang.com <sabg@paul-kang.com>
2018-08-26 18:43:57 +0800

    Initial Commit

---
+++ Magic8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic8
+//
+//  Created by Kang Paul on 2018/8/26.
+//  Copyright © 2018年 Kang Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
